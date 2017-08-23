dd07ee7c0c987a0d42322a4b944dcf362654bd50
ethwillz <ewwilliams6@gmail.com>
2017-08-23 16:29:21 -0500

    Cleaned up the CSS a little, working now on disappearing navbar on scroll and then reappearing on any scroll up

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,45 @@
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = 0;
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > navbarHeight){
+        // Scroll Down
+        $('#navbar').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() > $(document).height()) {
+            $('#navbar').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
 $(window).scroll(function(){
   //Personal information section fade in/out
-  if($(window).scrollTop() > 390){
+  if($(window).scrollTop() > 180){
     $("#programming").fadeIn();
     $("#fitness").fadeIn();
     $("#fashion").fadeIn();
@@ -12,9 +51,9 @@ $(window).scroll(function(){
   }
 
   //Quote fade in/out
-  if($(window).scrollTop() > 1410){
+  if($(window).scrollTop() > 1260){
     $("#quote").fadeIn();
-    if($(window).scrollTop() > 1525){
+    if($(window).scrollTop() > 1310){
       $("#author").fadeIn();
     }
   }
