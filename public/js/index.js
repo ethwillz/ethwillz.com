f52cfe6394b5ef92f85618629f96b4b6523c04b8
ethwillz <ewwilliams6@gmail.com>
2017-08-23 17:01:37 -0500

    Finished disappearing navbar, transitioning to working on redesigning projects page

--- public/js/index.js
+++ public/js/index.js
@@ -24,14 +24,12 @@ function hasScrolled() {
 
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
-    if (st > navbarHeight){
+    if (st > navbarHeight && st > lastScrollTop){
         // Scroll Down
         $('#navbar').removeClass('nav-down').addClass('nav-up');
     } else {
         // Scroll Up
-        if(st + $(window).height() > $(document).height()) {
-            $('#navbar').removeClass('nav-up').addClass('nav-down');
-        }
+        $('#navbar').removeClass('nav-up').addClass('nav-down');
     }
 
     lastScrollTop = st;
