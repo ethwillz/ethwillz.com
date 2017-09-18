a094184e6780978942e1d82ff3367a111385adac
ethwillz <ewwilliams6@gmail.com>
2017-09-18 13:02:34 -0500

    Git images fixed, need to figure out how to get them working better on dev server and not just in production build

--- src/index/index.js
+++ src/index/index.js
@@ -4,11 +4,23 @@ require('./index.scss')
 require('../resume/css/resume.scss')
 require('../images/main.jpg')
 require('../images/main2.jpg')
+require('../images/pattern.jpg')
 require('../images/home.png')
+require('../images/resume.png')
+require('../images/linkedin.png')
+require('../images/download.png')
+require('../images/facebook.png')
+require('../images/instagram.png')
+require('../images/location.png')
+require('../images/link.png')
+require('../images/programming.png')
+require('../images/fitness.png')
+require('../images/fashion.png')
+require('../images/twitter.png')
 
 $(window).scroll(function(){
   //Personal information section fade in/out
-  if($(window).scrollTop() > 180){
+  if($(window).scrollTop() > 130){
     $("#programming").fadeIn();
     $("#fitness").fadeIn();
     $("#fashion").fadeIn();
