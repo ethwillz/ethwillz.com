f9023219603f5cf096ee31831feb05ba523c4472
ethwillz <ewwilliams6@gmail.com>
2017-09-25 17:13:10 -0500

    Added skills section and linked GitHub

--- src/index/index.js
+++ src/index/index.js
@@ -5,18 +5,6 @@ require('../resume/resume.scss');
 require('../projects/projects.scss');
 
 $(window).scroll(function(){
-  //Personal information section fade in/out
-  if($(window).scrollTop() > 130){
-    $("#programming").fadeIn();
-    $("#fitness").fadeIn();
-    $("#fashion").fadeIn();
-  }
-  else{
-    $("#programming").fadeOut();
-    $("#fitness").fadeOut();
-    $("#fashion").fadeOut();
-  }
-
   //Quote fade in/out
   if($(window).scrollTop() > 1260){
     $("#quote").fadeIn();
